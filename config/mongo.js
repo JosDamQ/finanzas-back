60d50256e64a57d85612ad9df0346658ddee7bd4
JosDamQ <damian.garcia.12.2004@gmail.com>
2025-12-27 17:23:32 -0600

    Refactor MongoDB connection function to remove unused parameters and improve error handling

--- config/mongo.js
+++ config/mongo.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 const { env } = require('./env');
 
-exports.connect = async (req, res, next) => {
+exports.connect = async () => {
     try{
         const uriMongo = `${env.uriMongo}`;
         mongoose.set('strictQuery', false);
         await mongoose.connect(uriMongo);
         console.log('MongoDB connected');
     }catch(err) {
-        console.log(err);
-        next(err);
+        console.error('MongoDB connection error:', err);
+        process.exit(1); // Exit process with failure
     }
 }
