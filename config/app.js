41314bb3e291cb08774a5d9491f35149648b6c6e
JosDamQ <damian.garcia.12.2004@gmail.com>
2025-12-27 16:13:40 -0600

    Add budget and credit card routes to the Express app

--- config/app.js
+++ config/app.js
@@ -14,6 +14,8 @@ const userRoutes = require('../routes/userRoutes');
 const productRoutes = require('../routes/productRoutes');
 */
 const userRoutes = require('../src/modules/users/users.routes');
+const budgetRoutes = require('../src/modules/budgets/budgets.routes');
+const creditCardRoutes = require('../src/modules/creditCards/creditCards.routes');
 
 // Config server
 app.use(express.urlencoded({ extended: false }));
@@ -28,6 +30,8 @@ app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
 */
 app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/budgets', budgetRoutes);
+app.use('/api/v1/credit-cards', creditCardRoutes);
 
 // Test route
 app.post('/', (req, res) => {
--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const mongoconfig = require('./config/mongo');
 const app = require('./config/app');
+const cronService = require('./src/services/cron.service');
 
-mongoconfig.connect()
+mongoconfig.connect();
 app.initSever();
+cronService.initScheduledJobs();
